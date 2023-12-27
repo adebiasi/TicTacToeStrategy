afa5a3a36ecf5611567142961df7c4926a967d91
Alberto.Debiasi <A.Debiasi@pervoice.it>
2023-12-27 11:17:30 +0100

    Add minmax

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let prev_actions = []
 let currAssumption = 0;
 let assumption_stats_0;
 let assumption_stats_1;
-let assumption_stats_2;
+let assumption_stats_minmax;
 
 function extract_stats(prev_actions) {
     let num_p1_wins = 0;
@@ -63,6 +63,7 @@ function setup() {
             actions = data
             extract_stats(prev_actions);
             noLoop();
+
             redraw();
         }
     );
@@ -75,6 +76,7 @@ function changeAssumption() {
 
     currAssumption = ((currAssumption + 1) % assumptionTexts.length)
     // console.log(currAssumption)
+
     redraw();
 }
 
@@ -94,11 +96,11 @@ function calculateStats() {
 
     no_assumption_stats = [];
 
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            let spot = board[j][i];
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            let spot = board[row][col];
             if (spot != 'O' && spot != 'X') {
-                no_assumption_stats.push(extract_stats([...prev_actions, get_pos(i, j)]));
+                no_assumption_stats.push(extract_stats([...prev_actions, get_pos(col, row)]));
             } else {
                 no_assumption_stats.push([]);
             }
@@ -108,30 +110,27 @@ function calculateStats() {
 
     assumption_stats_0 = [];
     assumption_stats_1 = [];
-    assumption_stats_2 = [];
 
     let availableMoves = []
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            let spot = board[j][i];
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            let spot = board[row][col];
             if (spot != 'O' && spot != 'X') {
-                availableMoves.push(get_pos(i, j))
+                availableMoves.push(get_pos(col, row))
             }
         }
     }
 
 
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            let spot = board[j][i];
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            let spot = board[row][col];
             if (spot != 'O' && spot != 'X') {
                 let bestStats0 = null;
                 let bestStats1 = null;
-                let bestStats2 = null;
                 let best_value_0 = null;
                 let best_value_1 = null;
-                let best_value_2 = null;
-                let currPos = get_pos(i, j);
+                let currPos = get_pos(col, row);
                 for (let z = 0; z < availableMoves.length; z++) {
                     if (currPos != availableMoves[z]) {
                         let currStats = extract_stats([...prev_actions, currPos, availableMoves[z]]);
@@ -140,7 +139,6 @@ function calculateStats() {
                         let curr_tot = currStats[0] + currStats[1] + currStats[2];
                         let curr_value_0 = (curr_win) / curr_tot;
                         let curr_value_1 = (curr_win + currStats[2]) / curr_tot;
-                        let curr_value_2 = 10 * curr_win - 10 * curr_lose;
 
                         if (curr_tot != 0) {
 
@@ -152,10 +150,6 @@ function calculateStats() {
                                 bestStats1 = currStats;
                                 best_value_1 = curr_value_1;
                             }
-                            if (bestStats2 == null || curr_value_2 > best_value_2) {
-                                bestStats2 = currStats;
-                                best_value_2 = curr_value_2;
-                            }
                         }
                     }
                 }
@@ -163,29 +157,45 @@ function calculateStats() {
                     assumption_stats_0.push(bestStats0);
                 } else {
                     //last move case
-                    assumption_stats_0.push(extract_stats([...prev_actions, get_pos(i, j)]));
+                    assumption_stats_0.push(extract_stats([...prev_actions, get_pos(col, row)]));
                 }
                 if (bestStats1 != null) {
                     assumption_stats_1.push(bestStats1);
                 } else {
                     //last move case
-                    assumption_stats_1.push(extract_stats([...prev_actions, get_pos(i, j)]));
+                    assumption_stats_1.push(extract_stats([...prev_actions, get_pos(col, row)]));
                 }
-                if (bestStats2 != null) {
-                    assumption_stats_2.push(bestStats2);
-                } else {
-                    //last move case
-                    assumption_stats_2.push(extract_stats([...prev_actions, get_pos(i, j)]));
-                }
+
             } else {
                 assumption_stats_0.push([]);
                 assumption_stats_1.push([]);
-                assumption_stats_2.push([]);
             }
         }
     }
 
-    // console.log("calculate assumption_stats: " + assumption_stats)
+    assumption_stats_minmax = [];
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+
+            if (board[row][col] === '') {
+                let score
+                if (currentPlayer == 'O') {
+                    board[row][col] = 'O';
+                    score = minimax(board, 0, false);
+                    board[row][col] = '';
+                } else {
+                    board[row][col] = 'X';
+                    score = minimax(board, 0, true);
+                    board[row][col] = '';
+                }
+                assumption_stats_minmax.push(score);
+
+            } else {
+                assumption_stats_minmax.push([]);
+            }
+        }
+    }
+
 }
 
 function draw_grid(grid_width, grid_height) {
@@ -201,11 +211,11 @@ function draw_grid(grid_width, grid_height) {
     }
 
     // Disegna X e O
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            let x = i * w + w / 2;
-            let y = j * h + h / 2;
-            let spot = board[j][i];
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            let x = col * w + w / 2;
+            let y = row * h + h / 2;
+            let spot = board[row][col];
 
             if (spot == 'O') {
                 noFill();
@@ -225,51 +235,143 @@ function draw_grid(grid_width, grid_height) {
                     // stroke(255,0,0)
                     fill(255, 0, 0);
                 }
-                let assumption_stat_0 = assumption_stats_0[get_pos(i, j) - 1]
-                let assumption_stat_1 = assumption_stats_1[get_pos(i, j) - 1]
-                let assumption_stat_2 = assumption_stats_2[get_pos(i, j) - 1]
-                let no_assumption_stat = no_assumption_stats[get_pos(i, j) - 1]
+                let assumption_stat_0 = assumption_stats_0[get_pos(col, row) - 1]
+                let assumption_stat_1 = assumption_stats_1[get_pos(col, row) - 1]
+                let assumption_stat_minmax = assumption_stats_minmax[get_pos(col, row) - 1]
+                let no_assumption_stat = no_assumption_stats[get_pos(col, row) - 1]
 
                 let curr_value_0 = ((currentPlayer == 'X') ? assumption_stat_0[0] : assumption_stat_0[1]) / (assumption_stat_0[0] + assumption_stat_0[1] + assumption_stat_0[2]);
                 let curr_value_1 = (((currentPlayer == 'X') ? assumption_stat_1[0] : assumption_stat_1[1]) + assumption_stat_1[2]) / (assumption_stat_1[0] + assumption_stat_1[1] + assumption_stat_1[2]);
-                let curr_value_2 = 10 * ((currentPlayer == 'X') ? assumption_stat_2[0] : assumption_stat_2[1]) - 10 * ((currentPlayer == 'X') ? assumption_stat_2[1] : assumption_stat_2[0]);
 
                 textSize(16);
                 text(
                     "# possible ends\nX: " + no_assumption_stat[0] + " - O: " + no_assumption_stat[1] + " - tie: " + no_assumption_stat[2] +
                     "\n\nwith assumption 0\nX: " + assumption_stat_0[0] + " - O: " + assumption_stat_0[1] + " - tie: " + assumption_stat_0[2] + "(" + (curr_value_0 * 100).toFixed(2) + "%)" +
-                    "\nwith assumption 1\nX: " + assumption_stat_1[0] + " - O: " + assumption_stat_1[1] + " - tie: " + assumption_stat_1[2] + "(" + (curr_value_1 * 100).toFixed(2) + "%)"
-                    // "\nwith assumption 2\nX: " + assumption_stat_2[0] + " - O: " + assumption_stat_2[1] + " - tie: " + assumption_stat_2[2] + "(" + (curr_value_2) + ")"
-                    , x, y + h / 12);
+                    "\nwith assumption 1\nX: " + assumption_stat_1[0] + " - O: " + assumption_stat_1[1] + " - tie: " + assumption_stat_1[2] + "(" + (curr_value_1 * 100).toFixed(2) + "%)" +
+                    "\nwith minmax\nX: " + assumption_stat_minmax.score, x, y + h / 12);
             }
         }
     }
 }
 
+function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+        if (result === 'X wins!') {
+            return {score: -1};
+        } else if (result === 'O wins!') {
+            return {score: 1};
+        } else {
+            return {score: 0};
+        }
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        let move = {score: bestScore};
+
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (board[row][col] === '') {
+                    board[row][col] = 'O';
+                    let score = minimax(board, depth + 1, false).score;
+                    board[row][col] = '';
+                    if (score > bestScore) {
+                        bestScore = score;
+                        move = {score, index: [row, col]};
+                    }
+                }
+            }
+        }
+
+        return move;
+    } else {
+        let bestScore = Infinity;
+        let move = {score: bestScore};
+
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                if (board[row][col] === '') {
+                    board[row][col] = 'X';
+                    let score = minimax(board, depth + 1, true).score;
+                    board[row][col] = '';
+                    if (score < bestScore) {
+                        bestScore = score;
+                        move = {score, index: [row, col]};
+                    }
+                }
+            }
+        }
+
+        return move;
+    }
+}
+
+function checkWinner() {
+    // Check rows
+    for (let row = 0; row < 3; row++) {
+        if (board[row][0] === board[row][1] && board[row][1] === board[row][2] && board[row][0] !== '') {
+            return `${board[row][0]} wins!`;
+        }
+    }
+
+    // Check columns
+    for (let col = 0; col < 3; col++) {
+        if (board[0][col] === board[1][col] && board[1][col] === board[2][col] && board[0][col] !== '') {
+            return `${board[0][col]} wins!`;
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== '') {
+        return `${board[0][0]} wins!`;
+    }
+    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== '') {
+        return `${board[0][2]} wins!`;
+    }
+
+    // Check for a tie
+    let openSpots = 0;
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row][col] === '') {
+                openSpots++;
+            }
+        }
+    }
+    if (openSpots === 0) {
+        return 'Tie!';
+    }
+
+    return null; // No winner yet
+}
+
 function mousePressed() {
 
-    let i = floor(mouseX / (width / 3));
-    let j = floor(mouseY / (height / 3));
-    if (i >= 0 && j >= 0) {
-        if (board[j][i] == '') {
-            board[j][i] = currentPlayer;
+    let col = floor(mouseX / (width / 3));
+    let row = floor(mouseY / (height / 3));
+    if (col >= 0 && row >= 0) {
+        if (board[row][col] == '') {
+            board[row][col] = currentPlayer;
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-            prev_actions.push(get_pos(i, j));
+            prev_actions.push(get_pos(col, row));
+
             redraw();
         } else {
-            board[j][i] = '';
+            board[row][col] = '';
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
             prev_actions = prev_actions.filter(function (item) {
-                return item !== get_pos(i, j);
+                return item !== get_pos(col, row);
             });
+
             redraw();
         }
 
     }
 }
 
-function get_pos(i, j) {
-    let pos = 3 * (j) + (i + 1);
+function get_pos(col, row) {
+    let pos = 3 * (row) + (col + 1);
     return pos;
 }
 
